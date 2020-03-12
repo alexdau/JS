47710d2c9f23b441d889f199452bca7a79b466dc
Даудов Александр <xunder61@mail.ru>
2020-03-12 08:20:57 +0400

    ДЗ к 5 уроку. Готово только 3 и 4. В 4 не работает вторая кнопка почему-то

--- 5.js
+++ 5.js
@@ -1,54 +1,69 @@
-'use strict';
-const products = [
-    {
-        id: 3,
-        price: 127,
-        photos: [
-            "1.jpg",
-            "2.jpg",
-        ]
+let app = {
+    config:{
+    rows: [8, 7, 6, 5, 4, 3, 2, 1],
+    cols: [a, b, c, d, e, f, g, h]
+},
+
+run() {
+    letboard = this.generateBoard();
+},
+
+/**
+ * Метод генерирует игровое поле 8 на 8.
+ * @returns {string} html разметка в виде строки.
+*/
+generateBoard() {
+    let board = '';
+    let rowStartWithColor = 'white';
+    for (let i = 0; i < this.config.rows.length; i++) {
+        if (rowStartWithColor == 'white') {
+            row = this.generateRow(rowStartWithColor, this.config.rows[i]);
+            rowStartWithColor = 'black';
+        } else {
+            row = this.generateRow(rowStartWithColor, this.config.rows[i]);
+            rowStartWithColor = 'white';
+        }
+        board += row;
+    }
+    return `<table><tbody>${board}</tbody></table>`;
+},
+
+    /**
+     * Метод генерирует тег tr (строку игровой доски) с закрашенными тегами
+     * td (ячейками).
+     * @param {string} rowStartWithColor с какого цвета строка начинается от левого края,
+     * м.б. "white", "black".
+     * @param {number} rowNum номер строки от 8 до 1, т.к. шахматная доска формируется
+     * сверху вниз, поэтому номер начинается с 8.
+     * @returns {string} html-разметка, тег tr с оформленными внутри тегами td.
+     */
+    generateRow(rowStartWithColor, rowNum) {
+        let currentColorClass = rowStartWithColor;
+        let row = "";
+        for (let i = 0; i < this.config.cols.length; i++) {
+            let field = "";
+            if (currentColorClass === 'white') {
+                field = this.generateField('white', rowNum, this.config.cols[i]);
+                currentColorClass = 'blackField';
+            } else {
+                field = this.generateField('black', rowNum, this.config.cols[i]);
+                currentColorClass = 'white';
+            }
+            row += field;
+        }
+    return `<tr>${row}</tr>`;
     },
-    {
-        id: 5,
-        price: 499,
-        photos: []
-    },
-    {
-        id: 10,
-        price: 263,
-        photos: [
-            "3.jpg"
-        ]
-    },
-    {
-        id: 13,
-        price: 456,
-        photos: [
-            "3.jpg"
-        ]
-    },
-    {
-        id: 11,
-        price: 2326,
-        photos: [
-            "3.jpg"
-        ]
-    },
-    {
-        id: 12,
-        price: 226,
-        photos: [
-            "3.jpg"
-        ]
-    },
-    {
-        id: 8,
-        price: 78,
-    },
-];
-const prodWP = products.filter(item => "photos" in item && item.photos.length > 0);
-console.log(prodWP);
-const sortToPrice = products.sort(function(item1, item2) {
-    return  item1.price - item2.price;
-});
-console.log(sortToPrice);
+/**
+ * Метод генерирует ячейкум(тег td) с нужным классом цвета
+ * и координатами на поле.
+ * @param {string} color класс цвета ячейки м.б. "white", "black".
+ * @param {number} rowNum номер строки игровой доски.
+ * @param {string} colChar буква колонки игровой доски.
+ * @returns {string} html-разметка с заполненными атрибутами координат и классом цвета.
+ */
+    generateField(color, rowNum, colChar) {
+        return <td data-rownum="${rowNum}" data-colorchar>
+    }, 
+};
+
+app.run();
